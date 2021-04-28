0c4cae00206ecaa2680be2dffad756e2cbaffaad
RArbore <russel.jma@gmail.com>
2021-04-28 00:10:07 -0700

    Working triangle example

--- src/main.c
+++ src/main.c
@@ -4,6 +4,16 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include "../include/shaders/shaders.h"
+
+float vertices[] = {
+     0.0f,  0.5f, // Vertex 1 (X, Y)
+     0.5f, -0.5f, // Vertex 2 (X, Y)
+    -0.5f, -0.5f  // Vertex 3 (X, Y)
+};
+
+GLuint vao, vbo;
+
 void error_callback(int error, const char* description) {
     fprintf(stderr, "Error #%d: %s\n", error, description);
 }
@@ -27,15 +37,41 @@ GLFWwindow* initialize_window() {
 
 void render(GLFWwindow *window) {
     glClear(GL_COLOR_BUFFER_BIT);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
 
-
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
+}
+
+void load_shaders() {
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_source, NULL);
+    glCompileShader(vertex_shader);
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_source, NULL);
+    glCompileShader(fragment_shader);
+
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+
+    glLinkProgram(shader_program);
+    glUseProgram(shader_program);
+
+    GLint pos_attrib = glGetAttribLocation(shader_program, "position");
+    glVertexAttribPointer(pos_attrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(pos_attrib);
+}
+
+void handle_input() {
     glfwPollEvents();
 }
 
 void tick(GLFWwindow *window) {
     render(window);
+    handle_input();
 }
 
 int main(int argc, char** argv) {
@@ -46,11 +82,18 @@ int main(int argc, char** argv) {
 
     GLFWwindow *window = initialize_window();
 
-
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
     glfwSetFramebufferSizeCallback(window, resize_callback);
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    load_shaders();
 
     while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE)) {
         tick(window);
